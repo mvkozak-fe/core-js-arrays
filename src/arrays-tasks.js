cf364fc647aced4fa9fb1ac50aa70e3141506a85
mvkozak-fe <mvkozak.fe@gmail.com>
2024-02-10 00:04:32 +0300

    feat: add getIntervalArray task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -20,8 +20,11 @@
  *    getIntervalArray(0, 100) => [ 0, 1, 2, ..., 100 ]
  *    getIntervalArray(3, 3) => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  const res = [];
+  res.length = end - start + 1;
+  res.fill(0);
+  return Array.from(res, (item, i) => item + start + i);
 }
 
 /**
