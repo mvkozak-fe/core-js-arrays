16a98d689941cf606b6f2cbd0aff150aca05edb9
mvkozak-fe <mvkozak.fe@gmail.com>
2024-02-09 10:55:17 +0300

    feat: add getAverage task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -138,8 +138,10 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  const num =
+    arr.length > 0 ? arr.reduce((acc, cur) => acc + cur, 0) / arr.length : 0;
+  return Number.isInteger(num) ? num : +num.toFixed(2);
 }
 
 /**
